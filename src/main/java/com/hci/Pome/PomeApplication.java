bd22afcb04eb1b4fdc111e20aac0c7754ff64925
Liam-Jones <liamjones9@hotmail.com>
2020-01-30 14:26:35 -0400

    Initial Spring Project

---
+++ src/main/java/com/hci/Pome/PomeApplication.java
@@ -0,0 +1,13 @@
+package com.hci.Pome;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PomeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PomeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hci/Pome/PomeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hci.Pome;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PomeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
