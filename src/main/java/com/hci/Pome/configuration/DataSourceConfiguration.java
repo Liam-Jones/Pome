1ca8fab7d2c256c59e5b190a08cf040cce6b7761
Liam-Jones <liamjones9@hotmail.com>
2020-01-31 19:32:20 -0400

    Reset some configs, set up webjars, tested to make sure jquery was loading properly

--- src/main/java/com/hci/Pome/configuration/DataSourceConfiguration.java
+++ src/main/java/com/hci/Pome/configuration/DataSourceConfiguration.java
@@ -19,12 +19,17 @@ import javax.sql.DataSource;
 @Configuration
 public class DataSourceConfiguration {
 
+    private static final String TEMP_DIRECTORY = System.getProperty("java.io.tmpdir");
+
     @Bean(name = "mainDataSource")
     public DataSource createMainDataSource() {
 
         JdbcDataSource ds = new JdbcDataSource();
 
+        ds.setURL("jdbc:h2:"+TEMP_DIRECTORY+"/testdata;MODE=MySQL");
+
         return ds;
+
     }
 
 }
--- src/main/java/com/hci/Pome/configuration/WebConfiguration.java
+++ src/main/java/com/hci/Pome/configuration/WebConfiguration.java
@@ -3,16 +3,22 @@ package com.hci.Pome.configuration;
 import org.h2.server.web.WebServlet;
 import org.springframework.boot.web.servlet.ServletRegistrationBean;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-@Configuration
-public class WebConfiguration {
-
+public class WebConfiguration implements WebMvcConfigurer {
     @Bean
-    ServletRegistrationBean h2servletRegistration(){
+    ServletRegistrationBean h2servletRegistration() {
         ServletRegistrationBean registrationBean = new ServletRegistrationBean( new WebServlet());
         registrationBean.addUrlMappings("/h2/*");
         return registrationBean;
+    }
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry
+                .addResourceHandler("/webjars/**")
+                .addResourceLocations("classpath:/META-INF/resources/webjars/");
     }
 
 
