bfae4c9aed25117ae4ddd20ac39386dc5c4a7044
Liam-Jones <liamjones9@hotmail.com>
2020-01-31 09:57:32 -0400

    More basic configuration, checking to see views are working properly

--- src/main/java/com/hci/Pome/configuration/DataSourceConfiguration.java
+++ src/main/java/com/hci/Pome/configuration/DataSourceConfiguration.java
@@ -19,17 +19,12 @@ import javax.sql.DataSource;
 @Configuration
 public class DataSourceConfiguration {
 
-    private static final String TEMP_DIRECTORY = System.getProperty("java.io.tmpdir");
-
     @Bean(name = "mainDataSource")
     public DataSource createMainDataSource() {
 
         JdbcDataSource ds = new JdbcDataSource();
 
-        ds.setURL("jdbc:h2:"+TEMP_DIRECTORY+"/testdata;MODE=MySQL");
-
         return ds;
-
     }
 
 }
--- src/main/java/com/hci/Pome/controllers/AlbumController.java
+++ src/main/java/com/hci/Pome/controllers/AlbumController.java
@@ -2,10 +2,14 @@ package com.hci.Pome.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
-@RequestMapping(value = "/")
 public class AlbumController {
 
-
+    @RequestMapping(value = "/", method = RequestMethod.GET)
+    public String initialPage()
+    {
+        return "albumView";
+    }
 }
